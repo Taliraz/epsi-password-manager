3f238e646b46953e3362c95b166d68f708e138bd
Taliraz <sullyvan.armangau@gmail.com>
2021-06-18 10:31:59 +0200

    initial commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InscriptionComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
